ea4a6d103ddbd3258375931fd3aa1be58bee5890
annamaria99 <ams58@hi.is>
2021-02-02 19:22:04 +0000

    Mest öll virkni komin

--- app.js
+++ app.js
@@ -3,24 +3,26 @@ const path = require('path');
 
 const app = express();
 
-app.set('views','/views');
+const videos = require('./src/videos');
+
+app.set('views','./views');
 app.set('view engine', 'ejs');
 
-app.get('/', (req,res) => {
-    res.send('Hello World');
-});
+app.use(express.static('public'));
+
+app.use('/', videos);
 
 function notFoundHandler(req,res,next) { // eslint-disable-line
     const title = "Fannst ekki";
     const message = "Ó nei, efnið fannst ekki";
-    res.status(404).render('error',{title, message});
+    res.status(404).render('error', {title, message});
 }
 
 function errorHandler(err,req,res,next) { // eslint-disable-line
     console.error(err);
     const title = "Villa kom upp";
     const message = "";
-    res.status(500).render('error',{title,message});
+    res.status(500).render('error', {title, message});
 }
 
 app.use(notFoundHandler);
--- src/videos.js
+++ src/videos.js
@@ -1,21 +1,39 @@
+const util = require('util');
 const fs = require('fs');
 const express = require('express');
+const { nextTick } = require('process');
 
-fs.readFile('videos.json', 'utf8', (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+const router = express.Router();
+
+const readFileAsync = util.promisify(fs.readFile);
+
+async function readVideos() {
     try {
-        const videos = JSON.parse(data);
-        handleFile(videos);
-
-} catch(err) {
-        console.log(err);
+    const files = await readFileAsync('videos.json','utf8');
+    return JSON.parse(files);
     }
-})
-
-function handleFile(content) {
-    
-    
+    catch(e) {
+        throw new Error('error: ', e);
+    }
 }
+
+async function videoList(req, res) {
+    const title = 'Fræðslumyndbandaleigan';
+    const videodata = await readVideos();
+
+    res.render('videos', {title, videos: videodata});
+}
+
+async function video(req, res) {
+    const title = 'Myndband';
+    const id = req.params.id;
+
+    const videodata = await readVideos();
+    res.render('video', { title, id: id, video: videodata });
+ 
+}
+
+router.get('/', videoList);
+router.get('/test/:id', video);
+
+module.exports = router;
