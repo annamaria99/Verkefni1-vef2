51a8124459b7347ce0fe9a41e519dbc7eb12abb1
annamaria99 <ams58@hi.is>
2021-02-03 01:15:16 +0000

    Linterar keyra án villna

--- src/videos.js
+++ src/videos.js
@@ -1,36 +1,33 @@
 const util = require('util');
 const fs = require('fs');
 const express = require('express');
-const { nextTick } = require('process');
 
 const router = express.Router();
 
 const readFileAsync = util.promisify(fs.readFile);
 
 async function readVideos() {
-    try {
-    const files = await readFileAsync('videos.json','utf8');
+  try {
+    const files = await readFileAsync('videos.json', 'utf8');
     return JSON.parse(files);
-    }
-    catch(e) {
-        throw new Error('error: ', e);
-    }
+  } catch (e) {
+    throw new Error('error: ', e);
+  }
 }
 
 async function videoList(req, res) {
-    const title = 'Fræðslumyndbandaleigan';
-    const videodata = await readVideos();
+  const title = 'Fræðslumyndbandaleigan';
+  const videodata = await readVideos();
 
-    res.render('videos', {title, videos: videodata});
+  res.render('videos', { title, videos: videodata });
 }
 
 async function video(req, res) {
-    const title = 'Myndband';
-    const id = req.params.id;
+  const title = 'Myndband';
+  const { id } = req.params;
 
-    const videodata = await readVideos();
-    res.render('video', { title, id: id, video: videodata });
- 
+  const videodata = await readVideos();
+  res.render('video', { title, id, video: videodata });
 }
 
 router.get('/', videoList);
